597031935fc4abafcf0bedc98bded8c87c34703f
Romualdo Villalobos <romualdo.villalobos@wizard.com.co>
2018-12-07 11:38:30 -0500

    Improved documentations and added additional functions to mathUtils

--- MathUtils.js
+++ MathUtils.js
@@ -2,6 +2,7 @@ const DEG_180 = 180;
 const ZERO = 0;
 const ONE = 1;
 const TWO = 2;
+const FIVE = 5;
 const SIX = 6;
 const HEX = 16;
 const TEN = 10;
@@ -169,65 +170,128 @@ export class MathUtils {
 	}
 
 	/**
- * Get decimal digits of a number
- * @param {*} num
- */
-static decimalPlaces (num) {
-	var match = ('' + num).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
-	if (!match) { return ZERO; }
-	return Math.max(
-			ZERO,
-			// Number of digits right of decimal point.
-			(match[ONE] ? match[ONE].length : ZERO) -
-			// Adjust for scientific notation.
-			(match[TWO] ? +match[TWO] : ZERO));
-}
+	 * Get decimal digits of a number
+	 * @param {*} num
+	 */
+	static decimalPlaces (num) {
+		var match = ('' + num).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+		if (!match) { return ZERO; }
+		return Math.max(
+				ZERO,
+				// Number of digits right of decimal point.
+				(match[ONE] ? match[ONE].length : ZERO) -
+				// Adjust for scientific notation.
+				(match[TWO] ? +match[TWO] : ZERO));
+	}
 
 	/**
-	* Find prime factors of a number
-	*/
-	/*
+	 * Given a fraction with decimals like 62.5/30, obtain an expression equivalent
+	 * without decimal part in its terms, returns original fraction if not possible
+	 * @param {number} numerator the numerator of the fraction
+	 * @param {number} denominator the denominator of the fraction
+	 * @returns {object} Numerator and denominator without decimal part
+	 */
+	static equivalentFractionWithNoDecimals (numerator, denominator) {
+		const MAX_FACTOR = 10;
+		let currentNumerator = numerator;
+		let currentDenominator = denominator;
+
+		// find factor for numerator
+		let hasEnteredNumeratorLoop = false;
+		let factor = 1;
+		while (!Number.isInteger(currentNumerator) && factor <= MAX_FACTOR) {
+			currentNumerator = numerator * factor;
+			factor++;
+		}
+		// if numerator still has decimal part then return false
+		if (!Number.isInteger(currentNumerator)) return { isEquivalentWithoutDecimal: false, numerator: numerator, denominator: denominator }; ;
+
+		if (hasEnteredNumeratorLoop) {
+			const BEFORE_LAST_UPDATE = 1;
+			currentDenominator = denominator * (factor - BEFORE_LAST_UPDATE);
+		}
+
+		// find factor for denominator
+		let initialDenominator = currentDenominator;
+		let hasEnteredDenominatorLoop = false;
+		const ORIGINAL_FACTOR_VALUE = 1;
+		factor = ORIGINAL_FACTOR_VALUE;
+		while (!Number.isInteger(currentDenominator) && factor <= MAX_FACTOR) {
+			currentDenominator = initialDenominator * factor;
+			hasEnteredDenominatorLoop = true;
+			factor++;
+		}
+		// if denominator still has decimal part then return false
+		if (!Number.isInteger(currentDenominator)) return { isEquivalentWithoutDecimal: false, numerator: numerator, denominator: denominator };
+
+		if (hasEnteredDenominatorLoop) {
+			const BEFORE_LAST_UPDATE = 1;
+			currentNumerator = currentNumerator * (factor - BEFORE_LAST_UPDATE); // update numerator
+		}
+
+		return { isEquivalentWithoutDecimal: true, numerator: currentNumerator, denominator: currentDenominator };
+	}
+
+	/**
+	 * Chekc if a ginven number is int or float
+	 * @param {number} n number for checking
+	 * @returns {boolean} flag indicating if respective number is float or number
+	 */
+	static isInt (n) {
+		const ZERO = 0;
+		const ONE = 1;
+		return n % ONE === ZERO;
+	}
+
+	/**
+	 * Find prime factors of a number
+	 * @param {number} a number for searching its prime factors
+	 * @returns {Array} array containaining a set of prime factor for the given number
+	 */
 	static primeFactors (a) {
-		let primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997];
+		let primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]; // eslint-disable-line no-magic-numbers
 
 		var primeFactors = [];
 
 		// Trial division algorithm
-		for (var i = 0, p = primeNumbers[i]; i < primeNumbers.length && p * p <= a; i++ , p = primeNumbers[i]) {
-			while (a % p === 0) {
+		for (var i = ZERO, p = primeNumbers[i]; i < primeNumbers.length && p * p <= a; i++, p = primeNumbers[i]) {
+			while (a % p === ZERO) {
 				primeFactors.push(p);
 				a /= p;
 			}
 		}
 
-		if (a > 1) {
+		if (a > ONE) {
 			primeFactors.push(a);
 		}
 
 		return primeFactors;
 	}
-	*/
 
 	/**
-	* Analize fration  to  checking if have periodic decimals
-	*/
-	/*
+	 * Analyze fraction and check for periodic decimals
+	 * @param {number} n numerator of a given fraction
+	 * @param {number} d denominator of a given fraction
+	 * @returns {{repeat: boolean, integerPart : string, nonRepeatingPart: string, repeatingPart : string}} Object with four properties, where (1) 'repeat': is a boolean flag that indicates if a given fraction has periodic part (2) 'integerPart': is a string representing the integer part of the given fraction (3) 'nonRepeatingPart': is a string number representing the value of the decimal part that is not repeated (4) 'repeatingPart': is a string representing the value of the decimal part that is being repeated
+	 */
 	static getRepeatingDecimal (n, d) {
-		let pFS = primeFactors(d);
+		let pFS = MathUtils.primeFactors(d);
 		let outpuObj = {
 			value: n / d,
 			repeat: false
 		};
 
-		for (let i = 0; i < pFS.length; i++) { // Go through each of the denominators prime factors
-			if (pFS[i] !== 2 && pFS[i] !== 5) { // We have a repeating decimal
+		const MAX_DECIMAL_DIGIT_ANALYSIS = 20;
+
+		for (let i = ZERO; i < pFS.length; i++) { // Go through each of the denominators prime factors
+			if (pFS[i] !== TWO && pFS[i] !== FIVE) { // We have a repeating decimal
 				let output = [];
 				let ns = [];
 				let _n = n;
 
 				// Let's find the repeating decimal
 				// Repeating decimal algorithm - uses long division
-				for (let i = 0; i < 20; i++) { // For now find 20 spots, ideally this should stop after it finds the repeating decimal
+				for (let i = ZERO; i < MAX_DECIMAL_DIGIT_ANALYSIS; i++) { // For now find 20 spots, ideally this should stop after it finds the repeating decimal
 					// How many times does the denominator go into the numerator evenly
 					let val = _n / d;
 					let temp2 = parseInt(val);
@@ -236,8 +300,8 @@ static decimalPlaces (num) {
 						ns[_n] = i;
 					} else {
 						outpuObj.repeat = true;
-						outpuObj.integerPart = output.slice(0, 1).join('');
-						outpuObj.nonRepeatingPart = output.slice(1, ns[_n]).join('');
+						outpuObj.integerPart = output.slice(ZERO, ONE).join('');
+						outpuObj.nonRepeatingPart = output.slice(ONE, ns[_n]).join('');
 						outpuObj.repeatingPart = output.slice(ns[_n]).join('');
 
 						return outpuObj;
@@ -255,5 +319,4 @@ static decimalPlaces (num) {
 		// Terminating decimal
 		return outpuObj;
 	}
-	*/
 }
