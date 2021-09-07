9073da7271ed5453a5584b095e754df813f72703
AndresCasta <acastano@mindresearch.org>
2021-09-07 11:55:56 -0500

    Update NumberTag.js
    Fraction magic numbers correction

--- components/NumberTag.js
+++ components/NumberTag.js
@@ -2,10 +2,14 @@ import { MindPixiContainer } from 'mind-sdk/mindPixi/MindPixiContainer';
 import MindPixiText from 'mind-sdk/mindPixi/text/MindPixiText';
 import { MindPixiGraphics } from 'mind-sdk/mindPixi/MindPixiGraphics';
 import { MathUtils } from './MathUtils';
-import { COLOR } from '../Constants';
 
 const TAG_RADIUS = 5;
 const PADDING = 4;
+const ZERO = 0;
+const ONE = 1;
+const DIV_2 = 0.5;
+const MULT_2 = 2;
+const MULT_100 = 100;
 
 export class NumberTag extends MindPixiContainer {
 	constructor (numerator, denominator, usePercent, mindObjectOptions, useSingleValue = false, roundRect = false) {
@@ -41,12 +45,12 @@ export class NumberTag extends MindPixiContainer {
 		this.graphics = new MindPixiGraphics();
 		this.addChild(this.graphics);
 
-		this.textContainer = new MindPixiContainer({Rng: this.arena.Rng});
+		this.textContainer = new MindPixiContainer({ Rng: this.arena.Rng });
 		this.graphics.addChild(this.textContainer);
 
 		this.numeratorText = new MindPixiText('00', _textStyle);
 		this.textContainer.addChild(this.numeratorText);
-		this.numeratorText.anchor.set(0.5, 0);
+		this.numeratorText.anchor.set(DIV_2, ZERO);
 
 		this.denominatorText = new MindPixiText('00', _textStyle);
 		this.textContainer.addChild(this.denominatorText);
@@ -55,25 +59,25 @@ export class NumberTag extends MindPixiContainer {
 		this.textContainer.addChild(this.fractionLine);
 
 		if (this.usePercent) {
-			let percent = (this._numerator / this._denominator) * 100;
+			let percent = (this._numerator / this._denominator) * MULT_100;
 
 			this.numeratorText.text = '100%';
 			this.denominatorText.text = '';
 
 			this.denominatorText.y = 0;
 
-			this.numeratorText.x = this.textContainer.width / 2;
+			this.numeratorText.x = this.textContainer.width * DIV_2;
 
-			let roundRectWidth = this.textContainer.width + PADDING * 2;
-			let roundRectHeight = this.textContainer.height + PADDING * 2;
+			let roundRectWidth = this.textContainer.width + PADDING * MULT_2;
+			let roundRectHeight = this.textContainer.height + PADDING * MULT_2;
 
-			if (percent % 1 > 0) {
-				let percObj = MathUtils.getRepeatingDecimal(this._numerator * 100, this._denominator);
+			if (percent % ONE > ZERO) {
+				let percObj = MathUtils.getRepeatingDecimal(this._numerator * MULT_100, this._denominator);
 
 				if (percObj.repeat) {
 					let strToShow = percObj.integerPart + '.';
-					if (percObj.nonRepeatingPart.length > 0) {
-						strToShow += percObj.nonRepeatingPart[0];
+					if (percObj.nonRepeatingPart.length > ZERO) {
+						strToShow += percObj.nonRepeatingPart[ZERO];
 						this.fractionLine.clear();
 						this.numeratorText.text = strToShow + '%';
 					} else {
@@ -86,8 +90,8 @@ export class NumberTag extends MindPixiContainer {
 						let _style = this.arena.theme.getStyles('numberTag');
 						this.fractionLine.clear();
 						this.fractionLine.lineStyle(_style.stroke, _style.fill);
-						this.fractionLine.moveTo(0, 0);
-						this.fractionLine.lineTo(len, 0);
+						this.fractionLine.moveTo(ZERO, ZERO);
+						this.fractionLine.lineTo(len, ZERO);
 						this.addChild(this.fractionLine);
 						this.fractionLine.x = PADDING + posX;
 						this.fractionLine.y = PADDING;
@@ -97,52 +101,52 @@ export class NumberTag extends MindPixiContainer {
 					}
 				} else {
 					this.fractionLine.clear();
-					this.numeratorText.text = percent.toFixed(1).toString() + '%';
+					this.numeratorText.text = percent.toFixed(ONE).toString() + '%';
 				}
 			} else {
 				this.fractionLine.clear();
-				this.numeratorText.text = percent.toFixed(0).toString() + '%';
+				this.numeratorText.text = percent.toFixed(ZERO).toString() + '%';
 			}
 			this.denominatorText.text = '';
 
-			this.graphics.beginFill(0x000000, 1);
+			this.graphics.beginFill(_style.background, ONE);
 			if (this._roundRect) {
-				this.graphics.drawRoundedRect(0, 0, roundRectWidth, roundRectHeight, TAG_RADIUS);
+				this.graphics.drawRoundedRect(ZERO, ZERO, roundRectWidth, roundRectHeight, TAG_RADIUS);
 			} else {
-				this.graphics.drawRect(0, 0, roundRectWidth, roundRectHeight);
+				this.graphics.drawRect(ZERO, ZERO, roundRectWidth, roundRectHeight);
 			}
 		} else if (this.useSingleValue) {
 			this.denominatorText.text = '';
 
-			this.denominatorText.y = 0;
+			this.denominatorText.y = ZERO;
 
 			this.denominatorText.y = 0;
 
-			this.numeratorText.x = this.textContainer.width / 2;
+			this.numeratorText.x = this.textContainer.width * DIV_2;
 
-			let roundRectWidth = this.textContainer.width + PADDING * 2;
-			let roundRectHeight = this.textContainer.height + PADDING * 2;
+			let roundRectWidth = this.textContainer.width + PADDING * MULT_2;
+			let roundRectHeight = this.textContainer.height + PADDING * MULT_2;
 
 			this.numeratorText.text = this._numerator.toString();
 
-			this.graphics.beginFill(0x000000, 1);
+			this.graphics.beginFill(_style.background, ONE);
 			if (this._roundRect) {
-				this.graphics.drawRoundedRect(0, 0, roundRectWidth, roundRectHeight, TAG_RADIUS);
+				this.graphics.drawRoundedRect(ZERO, ZERO, roundRectWidth, roundRectHeight, TAG_RADIUS);
 			} else {
-				this.graphics.drawRect(0, 0, roundRectWidth, roundRectHeight);
+				this.graphics.drawRect(ZERO, ZERO, roundRectWidth, roundRectHeight);
 			}
 		} else {
 			this.denominatorText.y = this.numeratorText.textMeasurements.height;
 
-			let roundRectWidth = this.textContainer.width + PADDING * 2;
-			let roundRectHeight = this.textContainer.height + PADDING * 2;
+			let roundRectWidth = this.textContainer.width + PADDING * MULT_2;
+			let roundRectHeight = this.textContainer.height + PADDING * MULT_2;
 
-			this.graphics.beginFill(0x000000, 1);
+			this.graphics.beginFill(_style.background, ONE);
 
 			if (this._roundRect) {
-				this.graphics.drawRoundedRect(0, 0, roundRectWidth, roundRectHeight, TAG_RADIUS);
+				this.graphics.drawRoundedRect(ZERO, ZERO, roundRectWidth, roundRectHeight, TAG_RADIUS);
 			} else {
-				this.graphics.drawRect(0, 0, roundRectWidth, roundRectHeight);
+				this.graphics.drawRect(ZERO, ZERO, roundRectWidth, roundRectHeight);
 			}
 
 			this.numeratorText.text = this._numerator.toString();
@@ -150,19 +154,19 @@ export class NumberTag extends MindPixiContainer {
 
 			let lineWidth = Math.max(this.numeratorText.textMeasurements.width, this.denominatorText.textMeasurements.width);
 			this.fractionLine.lineStyle(_style.stroke, _style.fill);
-			this.fractionLine.moveTo(0, 0);
-			this.fractionLine.lineTo(lineWidth, 0);
+			this.fractionLine.moveTo(ZERO, ZERO);
+			this.fractionLine.lineTo(lineWidth, ZERO);
 
-			this.fractionLine.y = this.numeratorText.textMeasurements.height + this.fractionLine.height / 2;
+			this.fractionLine.y = this.numeratorText.textMeasurements.height + this.fractionLine.height * DIV_2;
 
-			this.numeratorText.x = this.textContainer.width / 2;
-			this.denominatorText.x = this.textContainer.width / 2 - this.denominatorText.textMeasurements.width / 2;
+			this.numeratorText.x = this.textContainer.width * DIV_2;
+			this.denominatorText.x = this.textContainer.width * DIV_2 - this.denominatorText.textMeasurements.width * DIV_2;
 		}
 
-		this.textContainer.x = this.graphics.width / 2 - this.textContainer.width / 2;
-		this.textContainer.y = this.graphics.height / 2 - this.textContainer.height / 2;
+		this.textContainer.x = this.graphics.width * DIV_2 - this.textContainer.width * DIV_2;
+		this.textContainer.y = this.graphics.height * DIV_2 - this.textContainer.height * DIV_2;
 
-		this.anchor = {x: 0.5, y: 0.5};
+		this.anchor = { x: DIV_2, y: DIV_2 };
 	}
 
 	get numerator () {
@@ -185,15 +189,15 @@ export class NumberTag extends MindPixiContainer {
 
 	reDraw () {
 		if (this.usePercent) {
-			let percent = (this._numerator / this._denominator) * 100;
+			let percent = (this._numerator / this._denominator) * MULT_100;
 
-			if (percent % 1 > 0) {
-				let percObj = MathUtils.getRepeatingDecimal(this._numerator * 100, this._denominator);
+			if (percent % ONE > ZERO) {
+				let percObj = MathUtils.getRepeatingDecimal(this._numerator * MULT_100, this._denominator);
 
 				if (percObj.repeat) {
 					let strToShow = percObj.integerPart + '.';
-					if (percObj.nonRepeatingPart.length > 0) {
-						strToShow += percObj.nonRepeatingPart[0];
+					if (percObj.nonRepeatingPart.length > ZERO) {
+						strToShow += percObj.nonRepeatingPart[ZERO];
 						this.numeratorText.text = strToShow + '%';
 						this.fractionLine.clear();
 					} else {
@@ -206,8 +210,8 @@ export class NumberTag extends MindPixiContainer {
 						let _style = this.arena.theme.getStyles('numberTag');
 						this.fractionLine.clear();
 						this.fractionLine.lineStyle(_style.stroke, _style.fill);
-						this.fractionLine.moveTo(0, 0);
-						this.fractionLine.lineTo(len, 0);
+						this.fractionLine.moveTo(ZERO, ZERO);
+						this.fractionLine.lineTo(len, ZERO);
 						this.addChild(this.fractionLine);
 						this.fractionLine.x = PADDING + posX;
 						this.fractionLine.y = PADDING;
@@ -217,24 +221,24 @@ export class NumberTag extends MindPixiContainer {
 					}
 				} else {
 					this.fractionLine.clear();
-					this.numeratorText.text = percent.toFixed(1).toString() + '%';
+					this.numeratorText.text = percent.toFixed(ONE).toString() + '%';
 				}
 			} else {
 				this.fractionLine.clear();
-				this.numeratorText.text = percent.toFixed(0).toString() + '%';
+				this.numeratorText.text = percent.toFixed(ZERO).toString() + '%';
 			}
 			this.denominatorText.text = '';
 
-			this.denominatorText.y = 0;
+			this.denominatorText.y = ZERO;
 
-			this.numeratorText.x = this.textContainer.width / 2;
+			this.numeratorText.x = this.textContainer.width * DIV_2;
 		} else if (this.useSingleValue) {
 			this.numeratorText.text = this._numerator.toString();
 			this.denominatorText.text = '';
 
-			this.denominatorText.y = 0;
+			this.denominatorText.y = ZERO;
 
-			this.numeratorText.x = this.textContainer.width / 2;
+			this.numeratorText.x = this.textContainer.width * DIV_2;
 		} else {
 			this.numeratorText.text = this._numerator.toString();
 			this.denominatorText.text = this._denominator.toString();
@@ -243,18 +247,18 @@ export class NumberTag extends MindPixiContainer {
 			let lineWidth = Math.max(this.numeratorText.textMeasurements.width, this.denominatorText.textMeasurements.width);
 			this.fractionLine.clear();
 			this.fractionLine.lineStyle(_style.stroke, _style.fill);
-			this.fractionLine.moveTo(0, 0);
-			this.fractionLine.lineTo(lineWidth, 0);
+			this.fractionLine.moveTo(ZERO, ZERO);
+			this.fractionLine.lineTo(lineWidth, ZERO);
 
-			this.fractionLine.y = this.numeratorText.textMeasurements.height + this.fractionLine.height / 2;
+			this.fractionLine.y = this.numeratorText.textMeasurements.height + this.fractionLine.height * DIV_2;
 			this.denominatorText.y = this.numeratorText.textMeasurements.height;
 
-			this.numeratorText.x = this.textContainer.width / 2;
-			this.denominatorText.x = this.textContainer.width / 2 - this.denominatorText.textMeasurements.width / 2;
+			this.numeratorText.x = this.textContainer.width * DIV_2;
+			this.denominatorText.x = this.textContainer.width * DIV_2 - this.denominatorText.textMeasurements.width * DIV_2;
 		}
 
-		this.textContainer.x = this.graphics.width / 2 - this.textContainer.width / 2;
-		this.textContainer.y = this.graphics.height / 2 - this.textContainer.height / 2;
+		this.textContainer.x = this.graphics.width * DIV_2 - this.textContainer.width * DIV_2;
+		this.textContainer.y = this.graphics.height * DIV_2 - this.textContainer.height * DIV_2;
 	}
 
 	destroy (options) {
@@ -266,6 +270,7 @@ export class NumberTag extends MindPixiContainer {
 	}
 
 	setPointer (x, y) {
+		let _style = this.arena.theme.getStyles('numberTag');
 		let triangleBase = 5;
 		if (this.parent === this.pointer.parent) {
 			this.pointer.clear();
@@ -273,33 +278,31 @@ export class NumberTag extends MindPixiContainer {
 			this.parent.addChild(this.pointer);
 			this.parent.addChild(this);
 		}
-		this.pointer.beginFill(0x000000, 1);
+		this.pointer.beginFill(_style.background, ONE);
 		let mPoints = [];
-		mPoints.push({x: 0, y: 0});
-		let mPoint2 = {x: 0, y: 0};
-		let mPoint3 = {x: 0, y: 0};
+		mPoints.push({ x: ZERO, y: ZERO });
+		let mPoint2 = { x: ZERO, y: ZERO };
+		let mPoint3 = { x: ZERO, y: ZERO };
 
 		let errorAprox = 0.001;
 
 		let orientation = '';
-		if (this.y + this.height / 2 > y + errorAprox) {
+		if (this.y + this.height * DIV_2 > y + errorAprox) {
 			orientation = 'DOWN ';
-		} else if (this.y + this.height / 2 < y - errorAprox) {
+		} else if (this.y + this.height * DIV_2 < y - errorAprox) {
 			orientation = 'UP ';
 		}
 
-		if (this.x + this.width / 2 > x + errorAprox) {
+		if (this.x + this.width * DIV_2 > x + errorAprox) {
 			orientation += 'RIGHT';
-		} else if (this.x + this.width / 2 < x - errorAprox) {
+		} else if (this.x + this.width * DIV_2 < x - errorAprox) {
 			orientation += 'LEFT';
 		}
 
-		// console.log(orientation);
-
-		mPoint2.x = (this.x + this.width / 2) - x;
-		mPoint2.y = (this.y + this.height / 2) - y;
-		mPoint3.x = (this.x + this.width / 2) - x;
-		mPoint3.y = (this.y + this.height / 2) - y;
+		mPoint2.x = (this.x + this.width * DIV_2) - x;
+		mPoint2.y = (this.y + this.height * DIV_2) - y;
+		mPoint3.x = (this.x + this.width * DIV_2) - x;
+		mPoint3.y = (this.y + this.height * DIV_2) - y;
 
 		switch (orientation) {
 			case 'DOWN ':
@@ -346,7 +349,7 @@ export class NumberTag extends MindPixiContainer {
 		mPoints.push(mPoint2);
 		mPoints.push(mPoint3);
 
-		this.pointer.moveTo(mPoints[0].x, mPoints[0].y);
+		this.pointer.moveTo(mPoints[ZERO].x, mPoints[ZERO].y);
 		for (let i = 1; i < mPoints.length; i++) {
 			let mPoint = mPoints[i];
 			this.pointer.lineTo(mPoint.x, mPoint.y);
@@ -369,10 +372,11 @@ export class NumberTag extends MindPixiContainer {
 
 export const fontStyles = {
 	'fontSize': 16,
-	'fill': COLOR.WHITE
+	'fill': '::styles.colorPalette.raw.colors.WHITE'
 };
 
 export const styles = {
-	'fill': COLOR.WHITE,
-	'stroke': 1.5
+	'fill': '::styles.colorPalette.raw.colors.WHITE',
+	'stroke': 1.5,
+	'background': '::styles.colorPalette.raw.colors.BLACK'
 };
